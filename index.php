0c705665155cd8ab59290e7f59da482acc88f3e9
Lucas Berg <33882240+Lucas243@users.noreply.github.com>
2017-11-22 00:48:29 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>LuftJet Airlines</title>
+<link rel="stylesheet" href="./styles/style.css" type="text/css" />
+</head>
+
+<body>
+
+<?php require("inc/menu.php"); ?>
+
+<?php require("inc/footer.php"); ?>
+</body>
+</html>
